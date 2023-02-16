4794d719968b53eb5d40e78979203b4c6dc10150
Sailin <amralnakib256@gmail.com>
2023-02-16 18:15:27 +0000

    output to stderr and return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Output to stderr 
+ *
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
