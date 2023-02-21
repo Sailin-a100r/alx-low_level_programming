f97993204a3d32457cc667dfafad3bff3cb086a3
Sailin <amralnakib256@gmail.com>
2023-02-21 19:24:21 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	long i;
+	long j;
+	int counter;
+
+	i = 1;
+	j = 2;
+	for (counter = 0; counter < 25; counter++)
+	{
+		printf("%li, ", i);
+		i += j;
+		printf("%li", j);
+		j +=i;
+		if (counter != 24)
+		{
+			printf(", ");
+		}
+	}
+	printf('\n');
+	return (0);
+}
