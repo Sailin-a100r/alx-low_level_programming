c28a1d15d238d0d3e046a9100c57add7e96834b2
Sailin <amralnakib256@gmail.com>
2023-02-21 19:36:44 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	long i;
+	long j;
+	int counter;
+	long sum;
+
+	sum = 0;
+	i = 1;
+	j = 2;
+	while (i <= 4000000 && j <= 4000000)
+	{
+		if (i % 2 == 0)
+		{
+			sum += i;
+		}
+		else if (j % 2 == 0)
+		{
+			sum += j;
+		}
+		i += j;
+		j += i;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
