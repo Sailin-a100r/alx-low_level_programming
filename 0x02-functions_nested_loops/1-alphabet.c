36194485b5a4537d98c7293b69a71caf968bae91
Sailin <amralnakib256@gmail.com>
2023-02-21 09:08:22 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lower case
+ *
+ */
+
+void print_alphabet(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
