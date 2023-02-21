3b23d27b932a8ba40492082575d63fdf0cc666f7
Sailin <amralnakib256@gmail.com>
2023-02-21 15:38:04 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-	for (int times = 0; times < 10; times++)
+	int times;
+	int i;
+
+	for (times = 0; times < 10; times++)
 	{
-		for (int i = 97; i <= 122; i++)
+		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
