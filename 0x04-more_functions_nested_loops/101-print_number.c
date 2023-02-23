31bb83be0b6c490f4ddbb6ec31820e1f5a3e56ef
Sailin <amralnakib256@gmail.com>
2023-02-23 19:53:11 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ *
+ * @n: number to print
+ *
+ */
+
+void print_number(int n)
+{
+	int i;
+	int j;
+
+	i = 10;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	while (i < n)
+	{
+		i *= 10;
+	}
+	i /= 10;
+	while (i > 0)
+	{
+		j = n / i;
+		_putchar('0' + j);
+		if (n >= 0)
+		{
+			n = n - (j * i);
+		}
+		else 
+		{
+			n = n + (j * i);
+		}
+		i /= 10;
+	}
+}
