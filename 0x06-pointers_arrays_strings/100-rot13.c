9040c8a0b47b6e625f9bef8e0b6ac8663862d768
Sailin <amralnakib256@gmail.com>
2023-03-02 09:28:39 +0000

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,14 +14,8 @@ char *rot13(char *str)
 	int i;
 	int j;
 	int length;
-	char letters[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z',
-	 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-	 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	char code[] = { 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z','a', 'b', 'c',
-		 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-		  'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K',
-		  'L', 'M'};
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char code[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	length = strlen(str);
 	for (i = 0; i < length; i++)
@@ -30,7 +24,8 @@ char *rot13(char *str)
 		{
 			if (str[i] == letters[j])
 			{
-				str[i] = cod[j];
+				str[i] = code[j];
+				break;
 			}
 		}
 	}
