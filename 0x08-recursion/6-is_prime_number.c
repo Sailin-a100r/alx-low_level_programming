d2bdb680900e516e669fce21d765dd8424d53654
Sailin <amralnakib256@gmail.com>
2023-03-08 19:26:22 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * is_prime_number - function that checks if the number is prime or not
@@ -10,14 +11,34 @@
 
 int is_prime_number(int n)
 {
-	int x;
-
-	if (n < 2 || n % 2 == 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (n == 2 || n == 3)
+	else
+	{
+		return (isprime(n, n - 1));
+	}
+}
+
+/**
+ * isprime - function that checks for prime numbers
+ *
+ * @num: number to check
+ * @var: number to check with
+ *
+ * Return: 1 if num is prime, 0 otherwise
+ */
+
+int isprime(int num, int var)
+{
+	if (var == 1)
 	{
 		return (1);
 	}
-		
+	if (num % var == 0)
+	{
+		return (0);
+	}
+	return (isprime(num, var - 1));	
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int squareroot(int a, int set);
+int isprime(int num, int var);
 
 #endif
