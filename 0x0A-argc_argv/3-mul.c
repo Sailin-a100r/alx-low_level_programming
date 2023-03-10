214b38fb34e62274aeefce02171740e65a5719f5
Sailin <amralnakib256@gmail.com>
2023-03-10 15:19:23 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,10 @@ int main(int argc, char *argv[])
 {
 	int i, j;
 
-	i = atoi(&*argv[1]);
-	j = atoi(&*argv[2]);
 	if (argc == 3)
 	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
 		printf("%d\n", i * j);
 		return (0);
 	}
