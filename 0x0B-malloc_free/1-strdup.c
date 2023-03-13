c6957b87e79d46d434899e746ef4d9b588d4633d
Sailin <amralnakib256@gmail.com>
2023-03-13 10:07:23 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	length = strlen(str);
-	copy = malloc(length * sizeof(char));
+	copy = malloc(length * sizeof(char) + 1);
 	if (copy == NULL)
 	{
 		return (NULL);
