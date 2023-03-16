44e10b62f7fb772321db476131e5e5730d240b2c
Sailin <amralnakib256@gmail.com>
2023-03-16 15:52:27 +0000

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,18 +15,23 @@ int *array_range(int min, int max)
 	int *array, diff, i;
 
 	if (min > max)
-		return (NULL);
-
-	diff = max - min + 1;
-	array = malloc(diff * sizeof(int));
-	if (array == NULL)
-		return (NULL);
-
-	i = 0;
-	while (diff > i)
 	{
-		array[i] = min + i;
-		i++;
+		return (NULL);
 	}
-	return (array);
+	else
+	{
+		diff = max - min + 1;
+		array = malloc(diff * sizeof(int));
+		if (array == NULL)
+		{
+			return (NULL);
+		}
+		i = 0;
+		while (diff > i)
+		{
+			array[i] = min + i;
+			i++;
+		}
+		return (array);
+	}
 }
