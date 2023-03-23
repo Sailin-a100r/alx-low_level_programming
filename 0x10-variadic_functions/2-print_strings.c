537fe36484619faa89a1d1ec7f27349038fb87a6
Sailin <amralnakib256@gmail.com>
2023-03-23 15:42:40 +0000

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			break;
 		}
 		printf("%s", string);
-		if (separator != NULL)
+		if (separator)
 		{
 			printf("%s", separator);
 		}
