e3733239ded270042c85d2547871ee351cce1178
Sailin-a100r <theinsan4444@gmail.com>
2023-04-07 20:31:37 +0000

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flib_bits - function that returns the number of bits
+ * flip_bits - function that returns the number of bits
  * you would need to flip to get from one number to another.
  *
  * @n: first number to change from
@@ -12,15 +12,16 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int num, i;
+	int mask, i;
+	unsigned int num;
 	unsigned long int filter;
 
 	num = 0;
 	filter  = n ^ m;
-
 	for (i = 0; i <= 63; i++)
 	{
-		if ((filter & (1 << i)) != 0)
+		mask = filter & (1 << i);
+		if (mask != 0)
 			num++;
 	}
 	return (num >> 1);
