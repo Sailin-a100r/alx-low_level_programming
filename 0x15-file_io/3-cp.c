2cf79b4c675fd82355b96060d8da0ca1013817ca
Sailin-a100r <theinsan4444@gmail.com>
2023-04-09 14:59:13 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,12 +27,19 @@ int main(int argc, char *argv[])
 	{
 		rres = read(fdf, buffer, 1024);
 		if (check_read_error(fdf, rres, argv[2]) == 1)
+		{
+			free(buffer);
 			exit(98);
+		}
 		fdt = open(argv[2], O_WRONLY | O_APPEND);
 		wres = write(fdt, buffer, rres);
 		if (check_write_error(fdt, wres, argv[1]) == 1)
+		{
+			free(buffer);
 			exit(99);
+		}
 	}
+	free(buffer);
 	if (close(fdf) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fdf);
